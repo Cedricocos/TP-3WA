238d6969dbf71dbc98802cc2c0ed71299b40813c
Cédric <cedric.olivry@sfr.fr>
2021-04-29 10:49:09 +0200

    Premier Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+import React from 'react';
+import Message from "./components/Message"
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      count: 10
+    }
+  }
+
+  render(){
+
+    return(
+      <div className="App-header">
+        <p>Je suis App, {this.state.count}</p>
+        <Message />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Message extends React.Component{
+
+  render(){
+    return(
+        <p>Je suis un message</p>
+    );
+  }
+}
+
+export default Message;
